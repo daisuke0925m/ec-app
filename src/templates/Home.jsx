22ebbda2b57f2bd9ed98d4de8bfdbd8bf2e77559
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-08-17 01:49:27 +0900

    updated

--- src/templates/Home.jsx
+++ src/templates/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import {getUserId, getUserName} from "../reducks/users/selectors";
+import { useSelector } from 'react-redux';
+
 
 const Home = () => {
+  const selector = useSelector(state => state);
+  const uid = getUserId(selector)
+  const username = getUserName(selector)
+
+
   return(
     <h2>
       HOME
+      <p>{uid}</p>
+  <p>{username}</p>
     </h2>
   )
 }
--- src/templates/Login.jsx
+++ src/templates/Login.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {useDispatch} from "react-redux";
-import {push} from "connected-react-router";
+import {signIn} from "../reducks/users/operations";
 
 const Login = () => {
   const dispatch = useDispatch();
   return(
     <div>
       <h2>ログイン</h2>
-      <button onClick={() => dispatch(push('/'))}>
+      <button onClick={() => dispatch(signIn())}>
         ログインする
       </button>
     </div>
