0908966ea010b44462ecb1c85524983d1779f559
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-08-16 19:05:39 +0900

    updated

--- src/reducks/store/store.js
+++ src/reducks/store/store.js
@@ -1,16 +1,22 @@
 import {
   createStore as reduxCreateStore,
   combineReducers,
+  applyMiddleware
 } from 'redux';
+import {connectRouter, routerMiddleware} from "connected-react-router"
 
 // import {ProdutsReducer} from '../products/reducers';
 import {UsersReducer} from '../users/reducers';
 
-export default function createStore() {
+export default function createStore(history) {
   return reduxCreateStore(
     combineReducers({
+      fouter: connectRouter(history),
       // products: ProductsReucers,
       users: UsersReducer,
-    })
+    }),
+    applyMiddleware(
+      routerMiddleware(history)
+    )
   )
 }
