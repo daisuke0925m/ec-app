3e803c1b06f1f751a2d7ca82ca1582d6eec85418
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-08-29 16:34:00 +0900

    created Auth component

--- src/reducks/users/actions.js
+++ src/reducks/users/actions.js
@@ -2,12 +2,7 @@ export const SIGN_IN = 'SIGN_IN';
 export const signInAction = (userState) => {
     return {
         type: 'SIGN_IN',
-        payload: {
-            isSignedIn: true,
-            role: userState.role,
-            uid: userState.uid,
-            username: userState.username
-        }
+        payload: userState
     }
 };
 
