aaa5fb954bdf96c664afce7e54b8c946d14eb906
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-09-19 20:37:51 +0900

    updated

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,3 +1,4 @@
-export { default as ImageArea } from './ImageArea'
-export { default as ImagePreview } from './ImageArea'
-export { default as SetSize } from './SetSizeArea'
+export { default as ImageArea } from './imageArea'
+export { default as ImagePreview } from './ImagePreview'
+export { default as SetSizeArea } from './SetSizeArea'
+export { default as ProductCard } from './ProductCard'
