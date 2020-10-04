37e05b7aca75213fb64f924f81b6e5752cceb74c
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-10-04 15:20:12 +0900

    注文履歴確認機能

--- src/components/Products/OrderHistoryItem.jsx
+++ src/components/Products/OrderHistoryItem.jsx
@@ -32,6 +32,9 @@ const OrderHistoryItem = (props) => {
             <TextDetail label={"注文日時"} value={orderedDatetime} />
             <TextDetail label={"発送予定日"} value={shippingDate} />
             <TextDetail label={"注文金額"} value={price} />
+            {order.products.length > 0 && (
+                <OrderedProducts products={order.products} />
+            )}
             <div className="module-spacer--extra-extra-small" />
             <Divider />
         </div>
